73a4700c812c6527d2251acdf57ad6f9405e9b93
mederic mesle <mederic.mesle@gmail.com>
2022-04-01 15:09:23 +0200

    New working

--- app/controllers/grinders_controller.rb
+++ app/controllers/grinders_controller.rb
@@ -4,4 +4,21 @@ class GrindersController < ApplicationController
     @grinders = Grinder.all
   end
 
+  def new
+    @grinder = Grinder.new
+  end
+
+  def create
+    @grinder = Grinder.new(grinder_params)
+    @grinder.save
+
+    redirect_to grinder_path
+  end
+
+  private
+
+  def grinder_params
+    params.require(:grinder).permit(:name, :model, :burr_type)
+  end
+
 end
