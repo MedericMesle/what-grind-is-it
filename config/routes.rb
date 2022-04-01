986519622f05de8bdbd839f79827ebcb7b2ec6bd
mederic mesle <mederic.mesle@gmail.com>
2022-04-01 14:52:19 +0200

    Ul list sorted

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'grinds/index', to: 'grinds#index'
   get 'grinds/about', to: 'grinds#about'
+  get 'grinders/index', to: 'grinders#index'
 end
